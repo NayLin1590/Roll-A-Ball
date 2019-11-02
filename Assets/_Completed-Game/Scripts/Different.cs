1e3a4068c67ef093676165413f41acf4d2d42d87
NayLin1590 <naylintbk15@gmail.com>
2019-11-02 16:09:11 +0700

    first commit

---
+++ Assets/_Completed-Game/Scripts/Different.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Different : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        Debug.Log("Fixed Update" + Time.deltaTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(" Update" + Time.deltaTime);
+    }
+    void LateUpdate()
+    {
+        Debug.Log("Late Update" + Time.deltaTime);
+    }
+}
---
+++ Assets/_Completed-Game/Scripts/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+
+// Include the namespace required to use Unity UI
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	// Create public variables for player speed, and for the Text UI game objects
+	public float speed;
+	public Text countText;
+	public Text winText;
+   
+	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+	private Rigidbody rb;
+	private int count;
+
+    //Renderer rend;
+	// At the start of the game..
+	void Start ()
+	{
+		// Assign the Rigidbody component to our private rb variable
+		rb = GetComponent<Rigidbody>();
+
+		// Set the count to zero 
+		count = 0;
+
+		// Run the SetCountText function to update the UI (see below)
+		SetCountText ();
+
+		// Set the text property of our Win Text UI to an empty string, making the 'You Win' (game over message) blank
+		winText.text = "";
+	}
+
+	// Each physics step..
+	void FixedUpdate ()
+	{
+		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		// Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
+		// multiplying it by 'speed' - our public player speed that appears in the inspector
+		rb.AddForce (movement * speed);
+	}
+
+	// When this game object intersects a collider with 'is trigger' checked, 
+	// store a reference to that collider in a variable named 'other'..
+	void OnTriggerEnter(Collider other) 
+	{
+		// ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+           // rend = other.GetComponent<Renderer>();
+
+            // Make the other game object (the pick up) to change color, to make it change color
+
+            other.gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+			//other.gameObject.SetActive (false);
+
+			// Add one to the score variable 'count'
+			count = count + 5;
+
+           
+			// Run the 'SetCountText()' function (see below)
+			SetCountText ();
+            other.gameObject.tag = "Finish Pick Up";
+		}
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("next"))
+        {
+
+            collision.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            collision.gameObject.tag = "touch";
+        }
+        else if (collision.gameObject.CompareTag ("touch"))
+        {
+           
+            collision.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            collision.gameObject.tag = "next";
+        }
+       
+        Debug.Log("Touch Cube" + collision.gameObject.name);
+    }
+    // Create a standalone function that can update the 'countText' UI and check if the required amount to win has been achieved
+    void SetCountText()
+	{
+		// Update the text field of our 'countText' variable
+		countText.text = "Count: " + count.ToString ();
+
+		// Check if our 'count' is equal to or exceeded 12
+		if (count >= 60 ) 
+		{
+			// Set the text value of our 'winText'
+			winText.text = "Congratutation!";
+		}
+	}
+   
+}
